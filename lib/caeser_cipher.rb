c7e301a995c8d331ad14d9be81b00540c03be868
vincemilo <vincemilo@gmail.com>
2021-11-04 14:52:42 -0600

    Linting

--- lib/caeser_cipher.rb
+++ lib/caeser_cipher.rb
@@ -1,37 +1,36 @@
 def caeser_cipher(string, shift)
-    
-    #convert string to ascii
-    ascii = string.bytes
+  # convert string to ascii
+  ascii = string.bytes
 
-    shifted = ascii.map do |e|
-        #check if alphabetic
-        if e.between?(65,90)
-            #wrap around for lowercase
-            if shift.positive?
-                e + shift > 90 ? (e + shift) - 26 : e + shift
-            else
-                e + shift < 65 ? (e + shift) + 26 : e + shift
-            end
-        elsif e.between?(97,122)
-            #wrap around for uppercase
-            if shift.positive?
-                e + shift > 122 ? (e + shift) - 26 : e + shift
-            else
-                e + shift < 97 ? (e + shift) + 26 : e + shift
-            end
-        else
-            e
-        end
+  shifted = ascii.map do |e|
+    # check if alphabetic
+    if e.between?(65, 90)
+      # wrap around for lowercase
+      if shift.positive?
+        e + shift > 90 ? (e + shift) - 26 : e + shift
+      else
+        e + shift < 65 ? (e + shift) + 26 : e + shift
+      end
+    elsif e.between?(97, 122)
+      # wrap around for uppercase
+      if shift.positive?
+        e + shift > 122 ? (e + shift) - 26 : e + shift
+      else
+        e + shift < 97 ? (e + shift) + 26 : e + shift
+      end
+    else
+      e
     end
+  end
 
-    rejoin = shifted.map { |e| e.chr }.join
-    p rejoin
+  rejoin = shifted.map(&:chr).join
+  p rejoin
 end
 
-print "Enter the message you would like to encode: "
+print 'Enter the message you would like to encode: '
 string = gets.chomp
 
-print "Enter shift factor: "
+print 'Enter shift factor: '
 shift = gets.chomp.to_i
 
 caeser_cipher(string, shift)
--- spec/caeser_cipher_spec.rb
+++ spec/caeser_cipher_spec.rb
@@ -1,8 +1,8 @@
 # spec/caeser_cipher.rb
-require './lib/caeser_cipher.rb'
+require './lib/caeser_cipher'
 
-describe "#caeser_cipher" do
-    it "Takes a string and a shift factor and outputs the modified string" do
-      expect(caeser_cipher("What a string!", 5)).to eql("Bmfy f xywnsl!")
-    end
+describe '#caeser_cipher' do
+  it 'Takes a string and a shift factor and outputs the modified string' do
+    expect(caeser_cipher('What a string!', 5)).to eql('Bmfy f xywnsl!')
+  end
 end
